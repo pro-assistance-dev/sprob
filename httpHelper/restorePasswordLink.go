a5e734e05c7142e60257dc6d785ff1db4f51d23f
LakkinziMusic <lakkinzimusic@gmail.com>
2022-03-25 10:07:30 +0300

    Http config

--- httpHelper/restorePasswordLink.go
+++ httpHelper/restorePasswordLink.go
@@ -3,5 +3,5 @@ package httpHelper
 import "fmt"
 
 func (i *HTTPHelper) GetRestorePasswordURL(userId string, uniqueId string) string {
-	return fmt.Sprintf("%s:%s/restore/password/%s/%s", i.Host, i.Port, userId, uniqueId)
+	return fmt.Sprintf("%s/restore/password/%s/%s", i.Host, userId, uniqueId)
 }
