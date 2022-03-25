ae015f35ec4659f64ce941fa5f014aeb12d36500
LakkinziMusic <lakkinzimusic@gmail.com>
2022-03-25 09:50:03 +0300

    Ip to domen

--- httpHelper/restorePasswordLink.go
+++ httpHelper/restorePasswordLink.go
@@ -2,10 +2,6 @@ package httpHelper
 
 import "fmt"
 
-func (i *HTTPHelper) GetRestorePasswordURL(userId string, uniqueId string) (string, error) {
-	ip, err := i.GetMyIP()
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("https://%s/restore/password/%s/%s", ip.String(), userId, uniqueId), nil
+func (i *HTTPHelper) GetRestorePasswordURL(userId string, uniqueId string, domen string) (string, error) {
+	return fmt.Sprintf("%s/restore/password/%s/%s", domen, userId, uniqueId), nil
 }
