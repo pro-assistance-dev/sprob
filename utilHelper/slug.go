629795820aa9e2809be5e7a9f86c8a7758082f38
LakkinziMusic <lakkinzimusic@gmail.com>
2022-05-03 15:35:13 +0300

    UniqueSlug

--- utilHelper/slug.go
+++ utilHelper/slug.go
@@ -1,9 +1,15 @@
 package utilHelper
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/gosimple/slug"
 )
 
-func (h *UtilHelper) MakeSlug(forSlug string) string {
-	return slug.Make(forSlug)
+func (h *UtilHelper) MakeSlug(forSlug string, unique bool) string {
+	s := slug.Make(forSlug)
+	if unique {
+		s = fmt.Sprintf("%s-%s", s, uuid.New())
+	}
+	return s
 }
