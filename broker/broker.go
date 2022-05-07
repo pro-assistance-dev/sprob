5e2e6992d280b303ab981af7de076854b7c17d73
LakkinziMusic <lakkinzimusic@gmail.com>
2022-05-07 11:06:02 +0300

    Chan size

--- broker/broker.go
+++ broker/broker.go
@@ -41,7 +41,7 @@ type (
 func NewBroker() (broker *Broker) {
 	// Instantiate a broker
 	b := &Broker{
-		notifier:       make(notifierChan, 1),
+		notifier:       make(notifierChan, 100000),
 		newClients:     make(chan notifierChan),
 		closingClients: make(chan notifierChan),
 		clients:        make(map[notifierChan]struct{}),
