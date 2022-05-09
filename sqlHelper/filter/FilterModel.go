8c72d770735da40798a44329ffbcaae0e79ec6cf
LakkinziMusic <lakkinzimusic@gmail.com>
2022-05-09 15:48:15 +0300

    Where id fix

--- sqlHelper/filter/FilterModel.go
+++ sqlHelper/filter/FilterModel.go
@@ -96,7 +96,7 @@ func (f *FilterModel) constructJoin(query *bun.SelectQuery) {
 		if f.Operator != In {
 			query = query.Where("?.? =? ", f.JoinTable, f.JoinTableIDCol, f.JoinTableID)
 		} else {
-			query = query.Where("?.?in  (?) ", f.JoinTable, f.JoinTableIDCol, f.JoinTableID)
+			query = query.Where("?.? in  (?) ", f.JoinTable, f.JoinTableIDCol, f.JoinTableID)
 		}
 		return
 	}
