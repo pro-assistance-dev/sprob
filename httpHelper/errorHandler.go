5a64067340b082f51685104f3c9007dfbd4a38c9
LakkinziMusic <lakkinzimusic@gmail.com>
2023-03-23 10:36:31 +0300

    NoRowsErrIgnore

--- httpHelper/errorHandler.go
+++ httpHelper/errorHandler.go
@@ -1,11 +1,13 @@
 package httpHelper
 
 import (
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (i *HTTPHelper) HandleError(c *gin.Context, err error, code int) bool {
-	if err != nil {
+	if err != nil && err.Error() != sql.ErrNoRows.Error() {
 		c.JSON(code, err.Error())
 		return true
 	}
