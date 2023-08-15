13503cce2b538b4c4332ca591aeb9aaa5c9ba702
LakkinziMusic <lakkinzimusic@gmail.com>
2023-08-15 20:44:38 +0300

    TokenClaimsAbstract

--- tokenHelper/main.go
+++ tokenHelper/main.go
@@ -35,48 +35,49 @@ type JWTClaimsSetter interface {
 	SetJWTClaimsMap(claims map[string]interface{})
 }
 
-func (h *TokenHelper) GetSigned(claims jwt.MapClaims) (string, error) {
+func (h *TokenHelper) getSigned(claims jwt.MapClaims) (string, error) {
 	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return rt.SignedString([]byte(h.TokenSecret))
 }
 
-func (item *TokenDetails) SetAccessTokenClaims(claims jwt.MapClaims, exp int) {
+func (item *TokenDetails) setAccessTokenClaims(claims jwt.MapClaims, exp int) {
 	claims["authorized"] = true
 	claims["access_uuid"] = uuid.NewString()
 	claims["exp"] = time.Now().Add(time.Minute * time.Duration(exp)).Unix()
 }
 
-func (item *TokenDetails) SetRefreshTokenClaims(claims jwt.MapClaims, exp int) {
+func (item *TokenDetails) setRefreshTokenClaims(claims jwt.MapClaims, exp int) {
 	claims["refresh_uuid"] = uuid.NewString()
 	claims["exp"] = time.Now().Add(time.Hour * time.Duration(exp)).Unix()
 }
 
 func (h *TokenHelper) CreateToken(claimsSetter JWTClaimsSetter) (td *TokenDetails, err error) {
 	atClaims := jwt.MapClaims{}
-	td.SetAccessTokenClaims(atClaims, h.TokenAccessMinutes)
+	td.setAccessTokenClaims(atClaims, h.TokenAccessMinutes)
 	claimsSetter.SetJWTClaimsMap(atClaims)
-	td.AccessToken, err = h.GetSigned(atClaims)
+	td.AccessToken, err = h.getSigned(atClaims)
 	if err != nil {
 		return nil, err
 	}
 
 	rtClaims := jwt.MapClaims{}
-	td.SetRefreshTokenClaims(rtClaims, h.TokenRefreshHours)
-	td.RefreshToken, err = h.GetSigned(rtClaims)
+	td.setRefreshTokenClaims(rtClaims, h.TokenRefreshHours)
+	td.RefreshToken, err = h.getSigned(rtClaims)
 	if err != nil {
 		return nil, err
 	}
 	return td, nil
 }
+
 func (h *TokenHelper) RefreshToken(refreshToken string, claimsSetter JWTClaimsSetter) (*TokenDetails, error) {
-	token, err := h.VerifyToken(refreshToken)
+	token, err := h.verifyToken(refreshToken)
 	if err != nil || !token.Valid {
 		return nil, err
 	}
 	return h.CreateToken(claimsSetter)
 }
 
-func (h *TokenHelper) VerifyToken(tokenString string) (*jwt.Token, error) {
+func (h *TokenHelper) verifyToken(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
@@ -89,8 +90,8 @@ func (h *TokenHelper) VerifyToken(tokenString string) (*jwt.Token, error) {
 	return token, nil
 }
 
-func (h *TokenHelper) extractTokenMetadata(r *http.Request, claim string) (string, error) {
-	token, err := h.VerifyToken(h.extractToken(r))
+func (h *TokenHelper) ExtractTokenMetadata(r *http.Request, claim string) (string, error) {
+	token, err := h.verifyToken(h.extractToken(r))
 	if err != nil {
 		return "", err
 	}
