5649d72169f77968c89c50326e6eff7ebeec880b
LakkinziMusic <lakkinzimusic@gmail.com>
2023-08-18 13:55:26 +0300

    Transaction

--- sqlHelper/main.go
+++ sqlHelper/main.go
@@ -1,11 +1,14 @@
 package sqlHelper
 
 import (
+	"context"
 	"fmt"
 )
 
 type SQLHelper struct {
 }
+
+type fqKey struct{}
 
 func NewSQLHelper() *SQLHelper {
 	return &SQLHelper{}
@@ -13,4 +16,15 @@ func NewSQLHelper() *SQLHelper {
 
 func (i *SQLHelper) WhereLikeWithLowerTranslit(col string, search string) string {
 	return fmt.Sprintf("WHERE lower(regexp_replace(%s, '[^а-яА-Яa-zA-Z0-9 ]', '', 'g')) LIKE lower(%s)", col, "'%"+search+"%'")
+}
+
+func (i *SQLHelper) InjectQueryFilter(c context.Context, q *QueryFilter) {
+	c = context.WithValue(c, fqKey{}, q)
+}
+
+func ExtractQueryFilter(ctx context.Context) *QueryFilter {
+	if i, ok := ctx.Value(fqKey{}).(*QueryFilter); ok {
+		return i
+	}
+	return nil
 }
