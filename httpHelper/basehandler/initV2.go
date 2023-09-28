74b79e5fe22b2e5b41cf288a14eae438e46232b8
LakkinziMusic <lakkinzimusic@gmail.com>
2023-09-28 20:04:14 +0300

    RemovePointerFromPluralMany

--- httpHelper/basehandler/initV2.go
+++ httpHelper/basehandler/initV2.go
@@ -16,7 +16,7 @@ type IServiceWithContext[TSingle, TPlural, TPluralWithCount any] interface {
 
 type IServiceWithManyWithContext[TSingle, TPlural, TPluralWithCount any] interface {
 	IService[TSingle, TPlural, TPluralWithCount]
-	UpsertMany(*TPlural) error
+	UpsertMany(TPlural) error
 	DeleteMany([]uuid.UUID) error
 }
 
@@ -31,6 +31,6 @@ type IRepositoryWithContext[TSingle, TPlural, TPluralWithCount any] interface {
 type IRepositoryWithManyWithContext[TSingle, TPlural, TPluralWithCount any] interface {
 	IRepository[TSingle, TPlural, TPluralWithCount]
 	Upsert(context.Context, *TSingle) error
-	UpsertMany(context.Context, *TPlural) error
+	UpsertMany(context.Context, TPlural) error
 	DeleteMany(context.Context, []uuid.UUID) error
 }
