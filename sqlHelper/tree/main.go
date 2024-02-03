21e338a3ce3c8244a5cd0f7b52689cb6d21be502
DmitriyKosterin <dancrimstudio@gmail.com>
2024-02-03 16:53:23 +0300

    ver6

--- sqlHelper/tree/main.go
+++ sqlHelper/tree/main.go
@@ -8,5 +8,6 @@ import (
 
 // CreateTree method
 func (item TreeModel) CreateTree(query *bun.SelectQuery) {
-	query = query.NewSelect().Model(&item).ColumnExpr(fmt.Sprintf("%s", item.getTableAndCols()))
+	query = query.NewSelect().Model(&item).ColumnExpr(item.getTableAndCols())
+	fmt.Println(query.String())
 }
