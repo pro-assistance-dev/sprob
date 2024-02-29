afae4dcc64c98d113095949526419260937af396
LakkinziMusic <lakkinzimusic@gmail.com>
2024-02-29 11:42:08 +0300

    map

--- middleware/ftsp.go
+++ middleware/ftsp.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"sync"
+
 	"github.com/google/uuid"
 	"github.com/pro-assistance/pro-assister/helpers/sql"
 )
@@ -9,16 +11,24 @@ type FTSPStore struct {
 	store map[string]sql.FTSP
 }
 
+var lock = sync.RWMutex{}
+
 var ftspStore = FTSPStore{store: make(map[string]sql.FTSP)}
 
 func (item FTSPStore) SetFTSP(query *sql.FTSPQuery) {
 	id := uuid.NewString()
 	query.FTSP.ID = id
 	query.QID = id
+
+	lock.RLock()
+	defer lock.RUnlock()
+
 	item.store[id] = query.FTSP
 }
 
 func (item FTSPStore) GetFTSP(qid string) (sql.FTSP, bool) {
+	lock.RLock()
+	defer lock.RUnlock()
 	ftsp, ok := item.store[qid]
 	return ftsp, ok
 }
