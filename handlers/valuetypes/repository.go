2e057e076e638c21d4217117b2573c861cca539a
LakkinziMusic <lakkinzimusic@gmail.com>
2024-03-05 10:35:05 +0300

    v1.0.5

--- handlers/valuetypes/repository.go
+++ handlers/valuetypes/repository.go
@@ -9,7 +9,6 @@ import (
 func (r *Repository) Get(c context.Context, id string) (*models.ValueType, error) {
 	item := models.ValueType{}
 	err := r.helper.DB.IDB(c).NewSelect().Model(&item).
-		Where("?TableAlias.id = ?", id).
 		WhereOr("?TableAlias.name = ?", id).
 		Scan(c)
 	return &item, err
