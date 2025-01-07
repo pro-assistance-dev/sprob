45a6c2cd3409731a8dc713d3a7b181a95ec19d26
lakkinzimusic <lakkinzimusic@gmail.com>
2025-01-07 18:54:09 +0300

    v1.0.111

--- modules/documents/migrations/20240814123300_init.up.sql
+++ modules/documents/migrations/20240814123300_init.up.sql
@@ -15,5 +15,5 @@ create table if not exists passport_scans (
     id uuid default uuid_generate_v4() not null,
     name varchar,
     file_info_id uuid,
-    item_order number
+    item_order numeric
 );
