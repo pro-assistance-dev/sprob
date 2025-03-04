a48c0e380788040c80172e0d651c2b2a570d373c
lakkinzimusic <lakkinzimusic@gmail.com>
2025-03-04 21:39:56 +0300

    v1.0.154

--- migrations/20241024151300_acts.up.sql
+++ migrations/20241024151300_acts.up.sql
@@ -1,8 +1,8 @@
-create table acts {
+create table acts (
   id uuid default uuid_generate_v4() not null primary key,
   user_id uuid,
   client_id uuid,
   item_id uuid,
-  created_at uuid,
-  updated_at uuid
-}
+  created_at timestamp,
+  updated_at timestamp
+)
