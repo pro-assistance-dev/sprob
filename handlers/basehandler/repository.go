2412fed73da2e0205ba6d0c03dd743057ed7b15d
lakkinzimusic <lakkinzimusic@gmail.com>
2025-07-27 21:24:40 +0300

    KeyValue

--- handlers/basehandler/repository.go
+++ handlers/basehandler/repository.go
@@ -2,6 +2,7 @@ package basehandler
 
 import (
 	"context"
+	"fmt"
 )
 
 func (r *Repository[T]) Create(c context.Context, item *T) (err error) {
@@ -14,10 +15,28 @@ type DbItemsWithCount[T any] struct {
 	Count int `json:"count"`
 }
 
+type IdLabel struct{
+
+}
+
+func (r *Repository[T]) IdLabel(c context.Context, labelCol string) (items []T, err error) {
+	var i []
+
+	colExpr := fmt.Sprintf("id, % as label", labelCol)
+
+	q := r.helper.DB.IDB(c).NewSelect().Model(&i).ColumnExpr(colExpr)
+
+	err = q.Scan(c)
+	items.Items = i
+
+	return items, err
+}
+
 func (r *Repository[T]) GetAll(c context.Context) (items DbItemsWithCount[T], err error) {
 	var i []T
-	q := r.helper.DB.IDB(c).NewSelect().
-		Model(&i)
+
+	q := r.helper.DB.IDB(c).NewSelect().Model(&i)
+
 	r.relation(q)
 	r.helper.SQL.ExtractFTSP(c).HandleQuery(q)
 
@@ -40,19 +59,6 @@ func (r *Repository[T]) Get(c context.Context, id string) (item T, err error) {
 	return item, err
 }
 
-func (r *Repository[T]) GetBySlug(c context.Context, slug string) (item T, err error) {
-	q := r.helper.DB.IDB(c).NewSelect().
-		Model(&item)
-
-	r.relation(q)
-
-	err = q.Where("?TableAlias.slug = ?", slug).Scan(c)
-	if err != nil {
-		return item, err
-	}
-	return item, err
-}
-
 func (r *Repository[T]) Delete(c context.Context, id string) (err error) {
 	_, err = r.helper.DB.IDB(c).NewDelete().Model((*T)(nil)).Where("id = ?", id).Exec(c)
 	return err
@@ -60,11 +66,6 @@ func (r *Repository[T]) Delete(c context.Context, id string) (err error) {
 
 func (r *Repository[T]) Update(c context.Context, item *T) (err error) {
 	_, err = r.helper.DB.IDB(c).NewUpdate().Model(item).WherePK().Exec(c)
-	return err
-}
-
-func (r *Repository[T]) UpdateMany(c context.Context, items []*T) (err error) {
-	_, err = r.helper.DB.IDB(c).NewUpdate().Model(&items).WherePK().Exec(c)
 	return err
 }
 
