04db325a0fe0beafc7623fb9d195dababfc430c9
lakkinzimusic <lakkinzimusic@gmail.com>
2025-11-29 14:21:59 +0300

    v1.0.201

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/uptrace/bun/driver/pgdriver v1.2.15
 	golang.org/x/crypto v0.40.0
 	golang.org/x/net v0.41.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
@@ -105,6 +106,7 @@ require (
 	golang.org/x/text v0.27.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	mellium.im/sasl v0.3.2 // indirect
--- helpers/email/main.go
+++ helpers/email/main.go
@@ -5,14 +5,13 @@ import (
 	"crypto/tls"
 	"encoding/base64"
 	"fmt"
-	"log"
 	"mime"
 	"mime/multipart"
-	"net"
-	"net/smtp"
 	"os"
 	"path/filepath"
 	"strings"
+
+	gomail "gopkg.in/gomail.v2"
 
 	"github.com/pro-assistance-dev/sprob/config"
 )
@@ -66,98 +65,23 @@ func (e *Email) SendEmailWithAttachments(to []string, subject string, body strin
 
 // SendEmail func
 func (e *Email) sendEmail() error {
-	auth := smtp.PlainAuth(
-		"",
+	m := gomail.NewMessage()
+	m.SetHeader("From", e.config.From)
+	m.SetHeader("To", e.request.To...)
+	m.SetHeader("Subject", e.request.Subject)
+	m.SetBody("text/html", e.request.Body)
+
+	d := gomail.NewDialer(
+		e.config.Server,
+		e.config.Port,
 		e.config.From,
 		e.config.Password,
-		e.config.Server,
 	)
-	if e.config.AuthMethod == string(LoginAuthMethod) {
-		auth = LoginAuth(e.config.From, e.config.Password)
-	}
-	body := e.request.ToBytes(e.config.From)
-	// header := map[string]string{}
-	// header["To"] = strings.Join(e.request.To, ",")
-	// header["From"] = e.config.From
-	// header["Subject"] = e.request.Subject
-	// header["MIME-Version"] = "1.0"
-	// header["Content-Type"] = "text/html; charset=\"utf-8\""
-	// // header["Content-Transfer-Encoding"] = "base64"
-	// message := ""
-	// for k, v := range header {
-	// 	message += fmt.Sprintf("%s: %s\r\n", k, v)
-	// }
-	// // message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(e.request.Body))
-	// message += "\r\n" + e.request.Body
-	//
-	// boundary := "\r"
-	// if e.request.Attachments.len > 0 {
-	// 	for k, v := range e.request.Attachments {
-	// 		buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
-	// 		buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
-	// 		buf.WriteString("Content-Transfer-Encoding: base64\n")
-	// 		buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
-	//
-	// 		b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
-	// 		base64.StdEncoding.Encode(b, v)
-	// 		buf.Write(b)
-	// 		buf.WriteString(fmt.Sprintf("\n--%s", boundary))
-	// 	}
-	//
-	// 	buf.WriteString("--")
-	// }
+	d.SSL = false
+	d.TLSConfig = &tls.Config{ServerName: e.config.Server}
 
-	servername := fmt.Sprintf("%s:%s", e.config.Server, e.config.Port)
-	host, _, _ := net.SplitHostPort(servername)
-
-	tlsconfig := &tls.Config{ //nolint:gosec
-		InsecureSkipVerify: false, //nolint:gosec
-		ServerName:         host,
-	}
-	conn, err := tls.Dial("tcp", servername, tlsconfig)
-	if err != nil {
+	if err := d.DialAndSend(m); err != nil {
 		return err
-	}
-	c, err := smtp.NewClient(conn, host)
-	if err != nil {
-		return err
-	}
-	// Auth
-	if err = c.Auth(auth); err != nil {
-		return err
-	}
-	// To && From
-	if err = c.Mail(e.config.From); err != nil {
-		return err
-	}
-	for _, t := range e.request.To {
-		if err = c.Rcpt(t); err != nil {
-			return err
-		}
-	}
-	// Data
-	w, err := c.Data()
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(body)
-	if err != nil {
-		return err
-	}
-	err = w.Close()
-	if err != nil {
-		return err
-	}
-	err = c.Quit()
-	if err != nil {
-		return err
-	}
-
-	if e.config.WriteTestFile {
-		err = os.WriteFile("./application-generate_send.html", body, 0o600)
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 
 	return nil
