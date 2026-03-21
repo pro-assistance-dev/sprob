b0d94768fe6ffe64145d3ff7d59d607262fe838c
lakkinzimusic <lakkinzimusic@gmail.com>
2026-03-21 11:49:28 +0300

    v1.0.232

--- config/config.go
+++ config/config.go
@@ -16,9 +16,9 @@ type Project struct {
 	Root string `mapstructure:"ROOT" yaml:"root"`
 
 	BinPath    string `mapstructure:"BIN_PATH" yaml:"bin_path"`
-	UploadPath string
+	UploadPath string `mapstructure:"UPLOAD_PATH" yaml:"upload_path"`
 
-	TemplatesPath string
+	TemplatesPath string `mapstructure:"TEMPLATES_PATH" yaml:"templates_path"`
 	ModelsPath    string `mapstructure:"MODELS_PATH" yaml:"models_path"`
 }
 
